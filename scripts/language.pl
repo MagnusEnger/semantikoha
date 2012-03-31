d15661215cf567cd10fa25da9bf7bd51ed4cbb74
Magnus Enger <magnus@enger.priv.no>
2012-03-31 12:28:34 +0200

    Make language.pl functional

--- scripts/language.pl
+++ scripts/language.pl
@@ -1,5 +1,27 @@
-# Placeholder for script to enrich triplestore with data from Lexvo
+#!/usr/bin/perl -w 
 
-SELECT DISTINCT ?lang WHERE {
-  GRAPH ?g { ?s <http://purl.org/dc/terms/language> ?lang . }
+# Find language URIs that have not been LOADed and LOAD them
+# Synopsis: language.pl 
+
+use lib '../';
+
+use Koha::LinkedData::Internal;
+use Modern::Perl;
+use Data::Dumper;
+
+my $query = 'SELECT DISTINCT ?lang ?label WHERE {
+  ?s <http://purl.org/dc/terms/language> ?lang . 
+  OPTIONAL { ?lang rdfs:label ?label . }
+  FILTER (!bound(?label))
+}';
+
+my $langs = Koha::LinkedData::get_sparql($query);
+
+foreach my $lang ( @{$langs} ) {
+
+  
+  if ($lang->{'lang'}->{'type'} && $lang->{'lang'}->{'type'} eq 'uri') {
+    Koha::LinkedData::Internal::verbose_load($lang->{'lang'}->{'value'});
+  }
+
 }
