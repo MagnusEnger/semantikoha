10b075dcd9c47c3f47421d4d78c362390e8ca3b3
Magnus Enger <magnus@enger.priv.no>
2012-04-08 23:31:00 +0200

    Remove Koha::LinkedData::Internal
    This breaks everything, cleanup to come!

--- Koha/LinkedData.pm
+++ Koha/LinkedData.pm
@@ -63,6 +63,38 @@ sub cgi_sparql {
 
 }
 
+# Load a URI into our own triplestore
+# Usage:
+# load(uri)
+
+sub sparql_insert {
+
+  my ($q) = @_;
+  return Koha::LinkedData::sparqlQuery($q, 'post');
+
+}
+
+sub verbose_load {
+
+  my ($newuri) = @_;
+
+  print "Loading $newuri\n";
+  my $loaded = Koha::LinkedData::Internal::load($newuri);
+  print "Loaded $loaded triples from $newuri\n";
+
+}
+
+sub load {
+
+  my ($uri) = @_;
+  
+  # TODO Check that $uri is a valid URI
+
+  my $loadquery = "LOAD <$uri>";
+  Koha::LinkedData::sparqlQuery($loadquery, 'post');
+
+}
+
 sub sparqlQuery {
   
   my ($sparql, $method, $baseURL, $baseURLkey, $debug) = @_;
