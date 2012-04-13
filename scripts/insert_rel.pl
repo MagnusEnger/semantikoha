2ca8a94bc7ce3e5d7ea8c5f3cc5c267352e8ac78
Magnus Enger <magnus@enger.priv.no>
2012-04-13 17:28:59 +0200

    Generalize scripts/insert_sameas.pl to scripts/insert_rel.pl
    Run scripts/insert_rel.pl without parameters to get usage, including
    available relationships.

--- scripts/insert_rel.pl
+++ scripts/insert_rel.pl
@@ -5,24 +5,32 @@
 
 use lib '../';
 
-use Koha::SPARQL qw( :DEFAULT :update );
+use Koha::SPARQL qw( :DEFAULT :update get_prefixes );
 use Data::Validate::URI qw( is_uri );
 use Getopt::Long;
 use Pod::Usage;
 use Modern::Perl;
 use Data::Dumper;
 
-my $usage = "Usage: insert_sameas.pl <uri1> <uri2>\n";
+my ($prefixes, $relations) = get_prefixes();
+
+my $usage = "Usage: perl insert_sameas.pl <uri1> <rel> <uri2>\n";
 
 my ($debug) = get_options();
 
 if ( !$ARGV[0] || !$ARGV[1] ) {
-  print "Not enough URIs given!\n$usage";
+  print "Not enough data given!\n";
+  print "Available relations:\n";
+  foreach my $key (keys %{$relations}) {
+    print "\t$key : " . $relations->{$key}->{'rel'} . " - " . $relations->{$key}->{'inv'} . "\n";
+  }
+  print "$usage";
   exit;
 }
 
 my $uri1 = $ARGV[0];
-my $uri2 = $ARGV[1];
+my $rel  = $ARGV[1];
+my $uri2 = $ARGV[2];
 
 if( !is_uri( $uri1 ) ) {
   print "$uri1 does not look like a URI!\n$usage";
@@ -32,10 +40,18 @@ if( !is_uri( $uri2 ) ) {
   print "$uri2 does not look like a URI!\n$usage";
   exit;
 }
+if( !$relations->{$rel} ) {
+  print "$rel is not recognized as a valid relation!\n";
+  exit;
+}
 
 my $args = {
-  'uri1' => $uri1,
-  'uri2' => $uri2,
+  'prefixes'  => $prefixes, # Bit of a hack, just include them all
+  'uri1'      => $uri1,
+  'rel'       => $relations->{$rel}->{'rel'}, 
+  'inv'       => $relations->{$rel}->{'inv'}, 
+  'uri2'      => $uri2,
+  'relations' => $relations,  
 };
 
 my $query = get_query('insert_sameas.query', $args);
